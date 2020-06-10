04cc3936278d4b869f42d8ad13d75cd4b4a33320
jiaojinxing <jiaojinxing@acoinfo.com>
2020-06-10 10:35:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## JSMN
+
+Jsmn is a world fastest JSON parser/tokenizer.
+
+License: MIT license.
+
+Download: `https://github.com/zserge/jsmn.git`
+
+Version: Commits on Jul 15 2019, Merge pull request #162 from ghane/Case-Warning
---
+++ src/ms_jsmn.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 MS-RTOS Team.
+ * All rights reserved.
+ *
+ * Detailed license information can be found in the LICENSE file.
+ *
+ * File: ms_jsmn.c JSON parser.
+ *
+ * Author: Jiao.jinxing <jiaojixing@acoinfo.com>
+ *
+ */
+
+#include "jsmn/jsmn.h"
